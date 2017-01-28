77755308bc3c8c40556a0291e3fc550bb97fa692
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-01-28 16:39:05 +0100

    webpack2 config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
+const fs = require('fs');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 const production = process.env.NODE_ENV === 'production';
 const hmr = process.argv.includes('--hot');
 
-const extractCSS = new ExtractTextPlugin('public/css/[name].css');
-
 module.exports = {
   entry: {
-    app: './assets/js/app.js'
+    app: [
+      './assets/js/app.js',
+      './assets/sass/app.scss'
+    ]
   },
   output: {
-    path: __dirname + '/public/js',
-    filename: '[name].js',
-    publicPath: '/js/'
+    path: hmr ? '/' : 'public',
+    filename: 'js/[name].js'
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: extractCSS.extract({
+        loader: ExtractTextPlugin.extract({
           fallbackLoader: 'style-loader',
           loader: [
             'css-loader',
@@ -35,11 +39,52 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader?cacheDirectory'
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]?[hash]',
+          publicPath: '/'
+        }
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg|otf)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]?[hash]'
+        }
+      }
     ],
   },
   plugins: [
-    extractCSS
+    new ExtractTextPlugin('css/[name].css'),
+    new CopyWebpackPlugin([
+      { from: 'node_modules/bootstrap-sass/assets/fonts/bootstrap', to: 'fonts' },
+      { from: 'node_modules/font-awesome/fonts', to: 'fonts' },
+      { from: 'node_modules/slick-carousel/slick/fonts', to: 'fonts' },
+      { from: 'node_modules/slick-carousel/slick/ajax-loader.gif', to: 'images' }
+    ]),
+    new FriendlyErrorsWebpackPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: production,
+      options: {
+        postcss: [
+          require('autoprefixer')
+        ],
+        context: __dirname,
+        output: { path: './' }
+      }
+    })
   ],
+  performance: {
+    hints: false
+  },
+  devtool: production ? '#source-map' : '#inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    compress: true
+  }
 };
 
 if (production) {
