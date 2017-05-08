73e50a92bf2f113552b7d0616c6b8cbe012762e0
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-05-08 15:38:54 +0200

    scripts inside app

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,20 @@ Vue.component('about', require('./components/Panel.vue'));
 const app = new Vue({
   el: '#app'
 });
+
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+$('button.sweet').click(function() {
+  swal({
+    title: 'Yo!',
+    text: 'Yeaaah!',
+    type: 'success',
+    confirmButtonText: 'Cool'
+  });
+});
