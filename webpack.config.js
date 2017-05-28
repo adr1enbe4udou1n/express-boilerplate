d62573d7f558326709eea0f33988d44d32442c8d
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-05-29 01:50:02 +0200

    fix vue devtools

--- webpack.config.js
+++ webpack.config.js
@@ -125,11 +125,6 @@ module.exports = {
       minChunks: Infinity
     }),
     extractSass,
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
     new BrowserSyncPlugin(
       {
         host: browserSyncHost,
@@ -176,6 +171,11 @@ if (production) {
   plugins = [
     new CleanWebpackPlugin(['dist'], {
       root: path.join(__dirname, '/public')
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
